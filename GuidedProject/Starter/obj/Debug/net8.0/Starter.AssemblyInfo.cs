5fe6632e1038344dffb4a8f25b07ea3c3e44ed1f
PJ Mantoss <joelptoss@gmail.com>
2024-05-24 08:08:02 +0100

    update gradeCalculator

--- GuidedProject/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
+++ GuidedProject/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73c8aeb13527e441669b51b107246c3b59488f1a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dac80a4543fa60f526b2534207bcc797e0ee4d49")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
